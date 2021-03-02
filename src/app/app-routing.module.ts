f388c34649bdcd39939bfecb3c52de70966fa881
Just Furbush <killjoy@Justs-MacBook-Air.local>
2021-03-01 19:59:46 -0500

    added main components and nav menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlphaComponent } from './alpha/alpha.component';
+import { AppComponent } from './app.component';
+import { CarComponent } from './car/car.component';
+import { DownloadComponent } from './download/download.component';
+import { HomeComponent } from './home/home.component';
+import { NoteComponent } from './note/note.component';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component : HomeComponent},
+  {path: '', component : AlphaComponent},
+  {path: 'download', component : DownloadComponent},
+  {path: 'note', component : NoteComponent},
+  {path: 'car', component : CarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { AlphaComponent } from './alpha/alpha.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { NoteComponent } from './note/note.component';
+import { DownloadComponent } from './download/download.component';
+import { CarComponent } from './car/car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidemenuComponent,
+    AlphaComponent,
+    NoteComponent,
+    DownloadComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
